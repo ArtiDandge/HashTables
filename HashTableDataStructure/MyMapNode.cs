c0d6edf8cb6368f1d6b61443650af69dad50a21e
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-17 19:29:50 +0530

    [Arti Dandge] Add : Ability to find frequency of word using HashTable

---
+++ HashTableDataStructure/MyMapNode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableDataStructure
+{
+    /// <summary>
+    /// MyMapNode is a Generic class and <K,V> generic types that this class is accepting 
+    /// </summary>
+    /// <typeparam name="K">K is Key </typeparam>
+    /// <typeparam name="V">V is Value</typeparam>
+    public class MyMapNode<K, V>
+    {
+        private readonly int size;
+        /// <summary>
+        /// LinkedList accepting array of generic type 'struct KeyValue<K,V>' structure  
+        /// </summary>
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        /// <summary>
+        /// Method to get position of key in the array using GetHashCode() method
+        /// </summary>
+        /// <param name="key">key is Key index for its value</param>
+        /// <returns></returns>
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        /// <summary>
+        /// Method to get value beased on its key
+        /// </summary>
+        /// <param name="key">key is Key index for its value</param>
+        /// <returns></returns>
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+
+        }
+
+        /// <summary>
+        /// Method to add Key-Value Pair to the linked list of type KeyValue<K,V>
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <param name="value">Value</param>
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+
+        /// <summary>
+        /// Method to get linked list based on its position
+        /// </summary>
+        /// <param name="position">position of linkedlist in array</param>
+        /// <returns></returns>
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        /// <summary>
+        /// A structure that has Key and Value
+        /// </summary>
+        /// <typeparam name="k">K is key</typeparam>
+        /// <typeparam name="v">V is Value</typeparam>
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+    }
+}
--- HashTableDataStructure/Program.cs
+++ HashTableDataStructure/Program.cs
@@ -7,6 +7,15 @@ namespace HashTableDataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("**********Hash Table Data Strcture***********");
+            MyMapNode<String, String> myHashMap = new MyMapNode<string, string>(5);
+            myHashMap.Add("0", "To");
+            myHashMap.Add("1", "be");
+            myHashMap.Add("2", "be");
+            myHashMap.Add("3", "be");
+            myHashMap.Add("4", "to");
+            myHashMap.Add("5", "be");
+            string hashS = myHashMap.Get("5");
+            Console.WriteLine(hashS);
         }
     }
 }
